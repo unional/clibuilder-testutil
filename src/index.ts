ce4dbb48d0d4ce21aee0adf7cadc33e617b98ec2
Homa Wong <homawong@gmail.com>
2017-06-23 01:28:17 -0700

    Add getPresenter to export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './createArgv'
 export * from './createCli'
+export * from './getPresenter'
 export * from './generateDisplayedMessage'
 export * from './InMemoryDisplay'
 export * from './InMemoryPresenter'
