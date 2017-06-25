a9783224f25cdf963939ba773a012f98c8abf61c
Homa Wong <homawong@gmail.com>
2017-06-25 01:27:13 -0700

    Update deps, default InMemPresenter.displayLevel to verbose

--- src/InMemoryPresenter.ts
+++ src/InMemoryPresenter.ts
@@ -1,7 +1,8 @@
-import { PlainPresenter } from 'clibuilder'
+import { PlainPresenter, DisplayLevel } from 'clibuilder'
 
 import { InMemoryDisplay } from './InMemoryDisplay'
 
 export class InMemoryPresenter extends PlainPresenter {
   display = new InMemoryDisplay()
+  displayLevel = DisplayLevel.Verbose
 }
