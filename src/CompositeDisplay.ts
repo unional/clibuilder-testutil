b0a73ada2943589ce752e8cb61ee7de2b170f151
Homa Wong <homawong@gmail.com>
2017-07-03 00:58:14 -0700

    Add spyDisplay, CompositeDisplay, echoCommand

---
+++ src/CompositeDisplay.ts
@@ -0,0 +1,21 @@
+
+import { Display } from 'clibuilder'
+
+export class CompositeDisplay implements Display {
+  displays: Display[]
+  constructor(...displays: Display[]) {
+    this.displays = displays
+  }
+  debug(...args: any[]): void {
+    this.displays.forEach(d => d.debug(...args))
+  }
+  info(...args: any[]): void {
+    this.displays.forEach(d => d.info(...args))
+  }
+  warn(...args: any[]): void {
+    this.displays.forEach(d => d.warn(...args))
+  }
+  error(...args: any[]): void {
+    this.displays.forEach(d => d.error(...args))
+  }
+}
